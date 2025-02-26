7fbdc211a094374db8d0e3e604f7a8f21a868618
Bubsa <bube.sim@gmail.com>
2025-02-26 20:47:25 +0100

    Class 02

---
+++ Class 02 App/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Class_02_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            string name = "Bube";
+
+            char character = "A" // tuka poso ima samo eden karakter vo dvojni navodnici ke ti frli greshka
+
+            string blabla = 'String' // tuka e istiot problem, stavash poise karakteri vo edinechni navodnici pak ti vadi greshka
+            
+            bool isValid = false;
+
+            long bigNumber = 13212316546;
+
+            float floatNumber = 1.2f; // kaj ova se stava f za indikacija deka e float ili floating precision
+
+            double doubleNumber = 1.234d; // tuka se stava d radi istoto pogore
+
+            object a = "Trajan";
+            object b = 123;
+            object c = true;
+            Console.ReadLine();
+        }
+    }
+}

811863a371a2d6db8e1aed7ba5bd187d8f8fd76c
Bubsa <bube.sim@gmail.com>
2025-03-11 19:10:25 +0100

    Homework #2
    With SumOfEven and the Students task

---
+++ Homeworks/Homework #2/SumOfEven/StudentGroup/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko"};
+            string[] studentsG2 = new string[] { "John", "Paul", "Ringo", "George", "and the fifth one"};
+
+            Console.WriteLine("Please input either 1 or 2");
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 1)
+            {
+                Console.WriteLine("The students in G1 are: ");
+                for (int i = 0; i < studentsG1.Length; i++)
+                {
+                    string students = studentsG1[i];
+                    Console.WriteLine("\n" + students);
+                }
+            }
+           if (input == 2)
+            {
+                Console.WriteLine("The students in G2 are: ");
+                for (int i = 0; i < studentsG2.Length; i++)
+                {
+                    string students = studentsG2[i];
+                    Console.WriteLine("\n" + students);
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/Homework #2/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] numArray = new int[6];
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                Console.WriteLine("Enter the numbers");
+                numArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                if (numArray[i] % 2 == 0)
+                {
+                    sum += numArray[i];
+                }
+            }
+            Console.WriteLine("The sum of even numbers is " + sum);
+        }
+    }
+}
